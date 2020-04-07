3273d3640e34f826a7760862b80ed41bfa8a332d
孔明 <310155154@qq.com>
2020-04-07 15:20:28 +0800

    first commit

---
+++ WaterWave/WaveView.h
@@ -0,0 +1,17 @@
+//
+//  WaveView.h
+//  WaterWave
+//
+//  Created by 明孔 on 2020/4/7.
+//  Copyright © 2020 明孔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WaveView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
